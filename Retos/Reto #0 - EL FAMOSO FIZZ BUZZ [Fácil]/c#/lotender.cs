9e52a87a1f29b0a36618f59fc141386d24b73b61
Kendall Guerra <kendall.guerra@redlab.la>
2024-10-27 23:46:48 -0500

    Reto#0 - C#

---
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/c#/lotender.cs
@@ -0,0 +1,48 @@
+﻿var numeroB = new List<int>();
+var numero3 = new List<int>();
+var numero5 = new List<int>();
+
+for (int i = 1; i <= 100; i++)
+{
+    numeroB.Add(i);
+}
+foreach (var item in numeroB)
+{
+    var total = item * 3;
+    if (total <= 100)
+    {
+        numero3.Add(total);
+    }
+}
+foreach (var item in numeroB)
+{
+    var total = item * 5;
+    if (total <= 100)
+    {
+        numero5.Add(total);
+    }
+}
+
+
+foreach (var numeroBase in numeroB)
+{
+    var fizz = numero3.Contains(numeroBase);
+    var Buzz = numero5.Contains(numeroBase);
+    if (fizz && Buzz)
+    {
+        Console.WriteLine($"FIZZ BUZZ {numeroBase}");
+    }
+    else if (fizz)
+    {
+        Console.WriteLine($"FIZZ {numeroBase}");
+    }
+    else if (Buzz)
+    {
+        Console.WriteLine($"BUZZ {numeroBase}");
+    }
+    else
+    {
+        Console.WriteLine(numeroBase);
+    }
+
+}
